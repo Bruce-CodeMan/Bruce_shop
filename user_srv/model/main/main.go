02bd590786c7e12ad2ba0d102d93a9c82d4387e4
Bruce <1033684650@qq.com>
2023-02-03 01:09:03 +0800

    同步user表

---
+++ user_srv/model/main/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+
+	"Bruce_shop/user_srv/model"
+)
+
+func main() {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/shop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // 慢查询，阈值
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // 这行代码可以将用户表设置成user
+		},
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+	_ = db.AutoMigrate(&model.User{})
+}
