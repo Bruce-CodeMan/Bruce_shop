74b85c48b01991cc3147abac6c2a49b1b5e6ac8e
Bruce <1033684650@qq.com>
2023-02-03 21:19:26 +0800

    完成用户列表接口

---
+++ user_srv/global/global.go
@@ -0,0 +1,38 @@
+package global
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func init() {
+	dsn := "root:12345678@tcp(127.0.0.1:3306)/shop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // 慢查询，阈值
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // 这行代码可以将用户表设置成user
+		},
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
--- user_srv/handler/user.go
+++ user_srv/handler/user.go
@@ -3,3 +3,70 @@
  * @Author: Bruce
  * @Description:
  */
+
+package handler
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+
+	"Bruce_shop/user_srv/global"
+	"Bruce_shop/user_srv/model"
+	"Bruce_shop/user_srv/proto"
+)
+
+type UserServer struct{}
+
+func Model2Response(user model.User) proto.UserInfoResponse {
+	// 在grpc的message中字段有默认值，不能随便赋值nil进去，容易出错
+	// 要搞清楚，哪些字段是有默认值
+	userInfoRsp := proto.UserInfoResponse{
+		Id:       user.ID,
+		Password: user.Password,
+		NickName: user.NickName,
+		Gender:   user.Gender,
+		Mobile:   user.Mobile,
+		Role:     int32(user.Role),
+	}
+	if user.Birthday != nil {
+		userInfoRsp.BirthDay = uint64(user.Birthday.Unix())
+	}
+	return userInfoRsp
+}
+
+func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page == 0 {
+			page = 1
+		}
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
+
+// 获取用户列表
+func (s *UserServer) GetUserList(ctx context.Context, req *proto.PageInfo) (*proto.UserListResponse, error) {
+	var users []model.User
+	result := global.DB.Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	resp := &proto.UserListResponse{}
+	resp.Total = int32(result.RowsAffected)
+
+	global.DB.Scopes(Paginate(int(req.Pn), int(req.PSize))).Find(&users)
+	for _, user := range users {
+		userInfoRsp := Model2Response(user)
+		// resp.Data = append(resp.Data, &(Model2Response(user)))   注意这样写是不行的，必须要声明一个变量来取地址
+		resp.Data = append(resp.Data, &userInfoRsp)
+	}
+	return resp, nil
+}
