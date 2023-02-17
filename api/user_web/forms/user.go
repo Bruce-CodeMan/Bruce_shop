0c7662a93821d7754d92cb47920403f3e54f9226
Bruce <1033684650@qq.com>
2023-02-17 21:31:41 +0800

    complete the registerForm about user

--- api/user_web/forms/user.go
+++ api/user_web/forms/user.go
@@ -12,3 +12,9 @@ type PasswordLoginForms struct {
 	Captcha   string `form:"captcha" json:"captcha" binding:"required,min=5,max=5"`
 	CaptchaId string `form:"captcha_id" json:"captcha_id" binding:"required"`
 }
+
+type RegisterForms struct {
+	Mobile   string `form:"mobile" json:"mobile" binding:"required,mobile"`
+	Password string `form:"password" json:"password" binding:"required,min=3,max=10"`
+	Code     string `form:"code" json:"code" binding:"required,min=6,max=6"`
+}
