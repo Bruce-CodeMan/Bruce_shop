ef433429479dd5163833bfc3c9fc497229819cf9
Bruce-CodeMan <1033684650@qq.com>
2023-02-21 13:15:03 +0800

    modified the main.go

--- srvs/user_srv/main.go
+++ srvs/user_srv/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	uuid "github.com/satori/go.uuid"
 	"net"
 
 	"github.com/hashicorp/consul/api"
+	uuid "github.com/satori/go.uuid"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
@@ -51,7 +51,7 @@ func main() {
 		panic(err)
 	}
 	check := &api.AgentServiceCheck{
-		GRPC:                           fmt.Sprintf("192.168.2.212:%d", *PORT),
+		GRPC:                           fmt.Sprintf("127.0.0.1:%d", *PORT),
 		Timeout:                        "5s",
 		Interval:                       "5s",
 		DeregisterCriticalServiceAfter: "15s",
@@ -63,7 +63,7 @@ func main() {
 	registration.ID = serviceID
 	registration.Port = *PORT
 	registration.Tags = []string{"Bruce", "Hsu"}
-	registration.Address = "192.168.2.212"
+	registration.Address = "127.0.0.1"
 	registration.Check = check
 
 	err = client.Agent().ServiceRegister(registration)
